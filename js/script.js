9cc83c2d6f658d292ce4c8ec07ee63cbf65dd207
lbell13131313-sudo <lbell13131313@gmail.com>
2026-03-26 20:16:49 -0400

    got my endpoint url

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=kZuBXqdiLwhstYJIjyFPZ3VyFXd2RruA&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
