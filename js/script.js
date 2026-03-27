f4bf9d9ceb55c5e2813e3adcb926ea10dd560513
lbell13131313-sudo <lbell13131313@gmail.com>
2026-03-26 20:42:07 -0400

    stored the dog gif data into an array and made the button functional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 console.log("script.js loaded");
 
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=kZuBXqdiLwhstYJIjyFPZ3VyFXd2RruA&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+// images array to hold the gifs
+let images = [];
+
+async function getDogGifs() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  images = data;
+
+  //console.log(data);
+  
+  // Update the DOM with the dog gif
+  const output = document.querySelector("gif-container"); 
+  // output.src = gif; // fix this
+}
+
+// button for recieving a dog gif
+const button = document.querySelector("fetch-gif-btn"); 
+
+button.addEventListener("click", function () { 
+  getDogGifs(); 
+});
